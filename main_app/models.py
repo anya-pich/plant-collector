e9320b0a3a73564a4cf808736cf8e455c5fadb53
anya-pich <anna.pich@outlook.com>
2020-04-08 16:33:52 -0700

    plants index mocked up

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Plant(models.Model):
+  name = models.CharField(max_length=100)
+  taxonomy = models.CharField(max_length=255, blank=True, null=True)
+  indoor = models.BooleanField()
+  outdoor = models.BooleanField()
+  SUN_LEVELS = (
+    ('SU', 'sun'),
+    ('PS', 'part shade'),
+    ('SH', 'shade'),
+  )
+  sun = models.CharField(
+    max_length=2,
+    choices=SUN_LEVELS
+  ) 
+  temperature_f = models.FloatField(blank=True, null=True)
+  purchase_date = models.DateField(blank=True, null=True)
+  diameter_i = models.FloatField(blank=True, null=True)
+  photo = models.ImageField(blank=True, null=True)
+  watering_frequency = models.DurationField()
--- main_app/views.py
+++ main_app/views.py
@@ -7,3 +7,25 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def plants_index(request):
+  return render(request, 'plants/index.html', {'plants': plants})
+
+class Plant:
+  def __init__(self, name, taxonomy, indoor, outdoor, sun, temperature_f, purchase_date, diameter_i, photo, watering_frequency):
+      self.name = name
+      self.taxonomy = taxonomy
+      self.indoor = indoor
+      self.outdoor = outdoor
+      self.sun = sun
+      self.temperature_f = temperature_f
+      self.purchase_date = purchase_date
+      self.diameter_i = diameter_i
+      self.photo = photo
+      self.watering_frequency = watering_frequency
+
+plants = [
+  Plant('Cactus', 'Cactaceae Mammilaria', True, False, 'SU', 54, 'Feb 2020', 4, 'photo', 'every week'),
+  Plant('Succulent', 'Succulent Crassula', True, False, 'SU', 54, 'Feb 2020', 4, 'photo', 'every week'),
+  Plant('Bushy thing', 'TBD', True, False, 'SU', 54, 'Feb 2020', 4, 'photo', 'every week'),
+]
