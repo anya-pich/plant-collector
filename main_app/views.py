60ec9304aa06304eadf358103ac16d123015da5f
anya-pich <anna.pich@outlook.com>
2020-04-09 16:46:19 -0700

    database set up

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Plant
 
 # Create your views here.
 def home(request):
@@ -9,23 +10,6 @@ def about(request):
   return render(request, 'about.html')
 
 def plants_index(request):
+  plants = Plant.objects.all()
   return render(request, 'plants/index.html', {'plants': plants})
 
-class Plant:
-  def __init__(self, name, taxonomy, indoor, outdoor, sun, temperature_f, purchase_date, diameter_i, photo, watering_frequency):
-      self.name = name
-      self.taxonomy = taxonomy
-      self.indoor = indoor
-      self.outdoor = outdoor
-      self.sun = sun
-      self.temperature_f = temperature_f
-      self.purchase_date = purchase_date
-      self.diameter_i = diameter_i
-      self.photo = photo
-      self.watering_frequency = watering_frequency
-
-plants = [
-  Plant('Cactus', 'Cactaceae Mammilaria', True, False, 'SU', 54, 'Feb 2020', 4, 'photo', 'every week'),
-  Plant('Succulent', 'Succulent Crassula', True, False, 'SU', 54, 'Feb 2020', 4, 'photo', 'every week'),
-  Plant('Bushy thing', 'TBD', True, False, 'SU', 54, 'Feb 2020', 4, 'photo', 'every week'),
-]
