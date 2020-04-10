52c13c5da024e88655d47bb21c595e9227f86ba4
anya-pich <anna.pich@outlook.com>
2020-04-09 22:12:25 -0700

    plant waterings added

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
 # class Plant(models.Model):
@@ -21,6 +22,11 @@ from django.db import models
 #   photo = models.ImageField(blank=True, null=True)
 #   watering_frequency = models.DurationField()
 
+CAT = (
+  ('W', 'Water'),
+  ('F', 'Plant food')
+)
+
 class Plant(models.Model):
   name = models.CharField(max_length=100)
   description = models.TextField(max_length=255)
@@ -29,3 +35,22 @@ class Plant(models.Model):
   watering_frequency = models.DurationField()
   def __str__(self):
     return self.name
+  def is_watered(self):
+    # last_watered = 
+    # time_elapsed = date.today() - last_watered
+    # self.watering_frequency > time_elapsed
+    return date.today() - self.watering_set.latest('date').date < self.watering_frequency
+
+class Watering(models.Model):
+  date = models.DateField('watering date')
+  cat = models.CharField(
+    'watering type',
+    max_length=1,
+    choices=CAT,
+    default=CAT[0][0]
+  )
+  plant = models.ForeignKey(Plant, on_delete=models.CASCADE)
+  def __str__(self):
+    return f"{self.get_cat_display()} on {self.date}"
+  class Meta:
+    ordering = ['-date']
--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
   path('about/', views.about, name='about'),
   path('plants/', views.plants_index, name='plants'),
   path('plants/<int:plant_id>/', views.plants_detail, name='detail'),
+  path('plants/<int:plant_id>/add_watering/', views.add_watering, name='add_watering'),
 ]
